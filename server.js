1177279676cbaf384eac838eddf178dc7fe59c85
Esteban Pereyra <esteba2000ar@hotmail.com>
2021-11-20 17:19:43 +0000

    agregue los motores de plantilla pug y ejs para mostrar los productos instalando las respectivas dependencias

--- server.js
+++ server.js
@@ -11,17 +11,29 @@ app.use(express.static('public'));
 app.use('/api/productos', productsRouter);
 
 app.engine('handlebars',engine());
+
+//MOTORES DE PLANTILLA
 app.set('views','./src/views');
-app.set('view engine','handlebars');
+app.set('view engine','handlebars'); //PARA UTILIZAR HANDLEBARS
+
+//PUG
+// app.set('views','./src/views/expressPUG');
+// app.set('view engine','pug'); //PARA UTILIZAR PUG
 
 //Indica que debe recibir JSON
 // app.use(express.json());
 // app.use(express.urlencoded({extended:true}));
 
+//EJS
+// app.set('views','./src/views/expressEJS');
+// app.set('view engine','ejs');
+
+
 const server = app.listen(PORT, () => {
     console.log(`Escuchando en el puerto ${PORT}`);
 })
 
+//HANDLEBARS
 app.get('/views/products',(req,res)=>{
     producto.getAllProducts()
     .then(result=>{
@@ -30,5 +42,31 @@ app.get('/views/products',(req,res)=>{
             productos : info
         }
         res.render('products',preparedObject)
+    })
+})
+
+//PUG
+
+app.get('/views/productspug',(req,res)=>{
+    producto.getAllProducts()
+    .then(result=>{
+        let info = result.payload;
+        let preparedObject ={
+            productos : info
+        }
+        res.render('productspug',preparedObject)
+    })
+})
+
+//EJS
+
+app.get('/views/productsejs',(req,res)=>{
+    producto.getAllProducts()
+    .then(result=>{
+        let info = result.payload;
+        let preparedObject ={
+            productos : info
+        }
+        res.render('productsejs',preparedObject)
     })
 })
